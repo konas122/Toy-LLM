736a53a63ee91feebb8023777657c27a20d7f020
konas122 <2407626470@qq.com>
2025-05-22 22:24:16 +0800

    inital commit

---
+++ gpt2/transformer.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+
+from gpt2.model import FeedForward, LayerNorm
+from gpt2.attention import MultiHeadAttention
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"], 
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"]
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x) # [batch_size, num_tokens, emb_size]
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        return x
---
+++ main.py
@@ -0,0 +1,82 @@
+import torch
+import tiktoken
+from utils import train_model
+from gpt2.gpt_model import GPTModel
+from dataset import create_dataloader
+
+
+if torch.cuda.is_available():
+    torch.set_default_device("cuda")
+
+
+"""
+GPT2-small (the 124M configuration we already implemented):
+    "emb_dim" = 768
+    "n_layers" = 12
+    "n_heads" = 12
+
+GPT2-medium:
+    "emb_dim" = 1024
+    "n_layers" = 24
+    "n_heads" = 16
+
+GPT2-large:
+    "emb_dim" = 1280
+    "n_layers" = 36
+    "n_heads" = 20
+
+GPT2-XL:
+    "emb_dim" = 1600
+    "n_layers" = 48
+    "n_heads" = 25
+"""
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 256,  # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
+}
+
+file_path = "data/the-verdict.txt"
+with open(file_path, "r", encoding="utf-8") as file:
+    text_data = file.read()
+
+train_ratio = 0.90
+split_idx = int(train_ratio * len(text_data))
+train_data = text_data[:split_idx]
+val_data = text_data[split_idx:]
+
+train_loader = create_dataloader(
+    train_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=True,
+    shuffle=True,
+    num_workers=0
+)
+
+val_loader = create_dataloader(
+    val_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0
+)
+
+model = GPTModel(GPT_CONFIG_124M)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
+tokenizer = tiktoken.get_encoding("gpt2")
+
+num_epochs = 10
+train_losses, val_losses, tokens_seen = train_model(
+    model, train_loader, val_loader, optimizer,
+    num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+    start_context="Every effort moves you", tokenizer=tokenizer
+)
